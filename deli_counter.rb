9893a8c5d033fbd5e0dd08a06c81e811c602e7bb
Nicole D <nnardella@gmail.com>
2018-10-23 14:46:35 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -25,9 +25,7 @@ end
 
 def take_a_number(katz_deli, name)
     katz_deli << name
-
-    katz_deli.each_with_index do |name,i|
-    puts "Welcome, #{name}. You are number #{i+1} in line."
+    puts "Welcome, #{name}. You are number #{katz_deli.length+1} in line."
     end
 
 end
