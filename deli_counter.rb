786dd0c5416e7cf9dec471c689b576aae095a881
Nicole D <nnardella@gmail.com>
2018-10-23 15:06:25 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -29,12 +29,12 @@ def take_a_number(katz_deli, name)
 end
 
 
-def now_serving(katz_deli, name)
+def now_serving(katz_deli)
   if katz_deli.empty?
    puts "There is nobody waiting to be served!"
 
   else
-    puts "Currently serving #{name}."
+    puts "Currently serving #{}."
     katz_deli.shift
   end
 
