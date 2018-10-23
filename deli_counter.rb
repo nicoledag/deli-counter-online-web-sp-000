020b680748d5e48d929bb2191be3a6d5682b212c
Nicole D <nnardella@gmail.com>
2018-10-23 14:09:52 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -41,9 +41,10 @@ def now_serving(katz_deli)
    puts "There is nobody waiting to be served!"
 
   else
+    
     katz_deli.each do |name|
     puts "Currently serving #{name}."
-    names.pop()
+    katz_deli.pop()
     end
   end
 
