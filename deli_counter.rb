1715459954685d9eac0c7aeba4d76cdba6c2551a
Nicole D <nnardella@gmail.com>
2018-10-23 14:59:00 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -34,7 +34,7 @@ def now_serving(katz_deli)
    puts "There is nobody waiting to be served!"
 
   else
-    puts "Currently serving #{name}."
+    puts "Currently serving #{katz_deli}."
     katz_deli.shift
   end
 
